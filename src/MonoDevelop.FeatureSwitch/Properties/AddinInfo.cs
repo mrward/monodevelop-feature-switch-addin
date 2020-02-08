0a47d15cc2682fc718b1c299896e4ad129cb646a
Matt Ward <matt.ward@microsoft.com>
2020-02-08 14:32:28 +0000

    Update addin to version 0.2

--- src/MonoDevelop.FeatureSwitch/Properties/AddinInfo.cs
+++ src/MonoDevelop.FeatureSwitch/Properties/AddinInfo.cs
@@ -28,7 +28,7 @@ using Mono.Addins;
 [assembly: Addin (
 	"FeatureSwitch",
 	Namespace = "MonoDevelop",
-	Version = "0.1",
+	Version = "0.2",
 	Category = "IDE extensions")]
 
 [assembly: AddinName ("Feature Switch")]
