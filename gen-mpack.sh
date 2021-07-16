08eb64a4791eeea8fd35433c2bbde2a6cf755cb0
Jose Medrano <josmed@microsoft.com>
2021-07-16 13:01:33 +0200

    make file instead sh

--- gen-mpack.sh
+++
@@ -1,2 +0,0 @@
-mono /Applications/Visual\ Studio\ \(Preview\).app/Contents/MonoBundle/vstool.exe setup pack bin/MonoDevelop.FeatureSwitch.dll
-mono /Applications/Visual\ Studio\ \(Preview\).app/Contents/MonoBundle/vstool.exe setup install ./MonoDevelop.FeatureSwitch_0.1.mpack
